557c3a10ba9a6a91cf4b9e4b97b81417e3c6edde
bjennings215 <>
2021-10-18 00:27:30 -0400

    added eductation/user stubs and review json

--- Education.java
+++ Education.java
@@ -1,3 +1,43 @@
+import java.util.Date;
+
 public class Education {
-    
+	String university;
+	String major;
+	String minor;
+	String location;
+	String concentration;
+	Date graduationDate;
+	GradeLevel gradeLevel;
+	
+	public Education() {
+		
+	}
+	
+	public String getUniversity() {
+		return university;
+	}
+	
+	public String getcampusLocation() {
+		return location;
+	}
+	
+	public String getMajor() {
+		return major;
+	}
+	
+	public String getMinor() {
+		return minor;
+	}
+	
+	public String getConcentration() {
+		return concentration;
+	}
+	
+	public Date getGradutationDate() {
+		return graduationDate;
+	}
+	
+	public GradeLevel getGradeLevel() {
+		return gradeLevel;
+	}
 }
--- User.java
+++ User.java
@@ -1,3 +1,17 @@
-public abstract class User {
-    
+
+public class User {
+	String username;
+	String password;
+	
+	public User() {
+		
+	}
+	
+	public boolean createAccount(String username, String password) {
+		return false;
+	}
+	
+	public boolean logIn(String username, String password) {
+		return false;
+	}
 }

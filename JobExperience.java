54266a55240250ec6f1abc6722f8c755fb13f335
Esamsartawi1 <70250191+Esamsartawi1@users.noreply.github.com>
2021-10-17 21:49:49 -0400

    additions from SCRUM 1-Esam

--- JobExperience.java
+++ JobExperience.java
@@ -1,3 +1,24 @@
+package InternshipApp;
+import java.util.Date;
+
 public class JobExperience {
-    
+    private String company;
+    private String cityLocation;
+    private String stateLocation;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    public String getCompany(){
+        return this.company;
+    }
+    public String getLocation(){
+        return this.cityLocation+this.stateLocation;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    public int getNumberOfMonths(){
+     return 0;   
+    }
 }
--- Review.java
+++ Review.java
@@ -1,3 +1,17 @@
 public class Review {
-    
+    private double numberRating;
+    private String writtenReview;
+
+    public Review(double numberRating,String writtenReview){
+
+    }
+    public double getNumberRating(){
+        return this.numberRating;
+    }
+    public String getWrittenReview(){
+        return this.writtenReview;
+    }
+    public String toString(){
+        return "";
+    }
 }

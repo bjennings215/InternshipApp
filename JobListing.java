9affb069cd7a56a170071174d86a5531a4a005d1
Adam Trickett <adamrt@email.sc.edu>
2021-10-17 21:03:30 -0400

    added methods for Student stub class

--- JobListing.java
+++ JobListing.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+
 public class JobListing {
     
+    private ArrayList<Review> jobReviews;
+
+    public ArrayList<Review> getJobReviews() {
+        return this.jobReviews;
+    }    
 }
--- Student.java
+++ Student.java
@@ -1,3 +1,82 @@
-public class Student {
+import java.util.ArrayList;
+
+public class Student extends User {
     
+    private Resume resume;
+    private boolean employementStatus;
+    private ArrayList<JobListing> favoriteJobs;
+    private ArrayList<Review> reviewsMade;
+
+    public Student(String username, String password) {
+
+    }
+
+    public Resume getResume() {
+        return this.resume;
+    }
+
+    public boolean getEmployementStatus() {
+        return true;
+    }
+
+    public ArrayList<JobListing> getFavoriteJobs() {
+        return this.favoriteJobs;
+    }
+
+    public ArrayList<Review> getReviewsMade() {
+        return this.reviewsMade;
+    }
+
+    public void createResume() {
+
+    }
+
+    public void editResume() {
+
+    }
+
+    public boolean changeUsername() {
+        return true;
+    }
+
+    public boolean changePassword() {
+        return true;
+    }
+
+    public boolean changeEmployementStatus() {
+        return true;
+    }
+
+    public boolean addFavorite() {
+        return true;
+    }
+
+    public boolean removeFavorite() {
+        return true;
+    }
+
+    public void giveJobReview(JobListing jobListing) {
+
+    }
+
+    public boolean deleteJobReview(Review review) {
+        return true;
+    }
+
+    public JobListing searchJobListings(JobListing jobListing) {
+        return jobListing;
+    }
+
+    public ArrayList<Review> seeJobListingReviews(JobListing jobListing) {
+        return jobListing.getJobReviews();
+    }
+
+    public void filterJobListings(JobFilter jobFilter) {
+
+    }
+
+    public boolean applyToJob(JobListing jobListing) {
+        return true;
+    }
+
 }

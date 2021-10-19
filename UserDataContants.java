6f6484e8ff62550cf13df7fbf08305151ee88976
bjennings215 <>
2021-10-19 13:49:24 -0400

    added dataconstants/writer/loader added User singleton

---
+++ UserDataContants.java
@@ -0,0 +1,7 @@
+
+public abstract class UserDataContants {
+    protected static final String USER_FILE_STRING = "InternshipApp/User.json";
+    protected static final String USERS_STRING = "users";
+    protected static final String USER_USERNAME_STRING = "username";
+    protected static final String USER_PASSWORD_STRING = "password";
+}

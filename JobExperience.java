5b3bad121f78929d712135a451a52fa4dc60e1bb
Esam Sartawi <esartawi@email.sc.edu>
2021-10-21 14:24:49 -0400

    add get months in job experience

--- JobExperience.java
+++ JobExperience.java
@@ -1,5 +1,5 @@
-
 import java.util.Date;
+import java.util.Calendar;
 
 public class JobExperience {
     private String company;
@@ -19,6 +19,15 @@ public class JobExperience {
         return this.description;
     }
     public int getNumberOfMonths(){
-     return 0;   
+        int numberofMonths;
+        Calendar first=Calendar.getInstance();
+        first.setTime(startDate);
+        int firstMonth=first.get(Calendar.MONTH); //this will give the month of the start date  
+
+        Calendar second=Calendar.getInstance();
+        first.setTime(startDate);
+        int secondMonth=second.get(Calendar.MONTH);
+        numberofMonths=secondMonth-firstMonth; //this will giv the month of the end date
+        return numberofMonths;   
     }
 }
